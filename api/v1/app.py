89d51b709140bd4419061a56ee2825229a271922
Fares84 <fares.sassi2015@gmail.com>
2021-05-07 13:06:46 +0000

    updated files

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module for Flask REST application """
+
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """ close databse session """
+    storage.close()
+
+if __name__ == "__main__":
+    """ main fucntion """
+    app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
+            port=getenv('HBNB_API_PORT', default=5000), threaded=True)
