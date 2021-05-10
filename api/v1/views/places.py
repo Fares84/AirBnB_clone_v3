a31a394a3ae25aac977b6420c21e75b2da024035
Fares84 <fares_sassi@outlook.fr>
2021-05-10 12:39:20 +0000

    fixed file pep8 issue

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""City objects that handles all default RestFul API actions """
+""" create a new view for City objects that handles all default RestFul API """
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
@@ -7,16 +7,12 @@ from models.place import Place
 from models.city import City
 
 
-@app_views.route(
-    '/cities/<city_id>/places',
-    methods=['GET'],
-    strict_slashes=False)
-@app_views.route(
-    '/places/<place_id>',
-    methods=['GET'],
-    strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
 def cities_places(city_id=None, place_id=None):
-    """place objects that handles all default RestFul API actions"""
+    """ retrieve Place's list obj of City and retrieve info from given place"""
     if place_id:
         if storage.get(Place, place_id):
             return jsonify(storage.get(Place, place_id).to_dict())
@@ -34,11 +30,10 @@ def cities_places(city_id=None, place_id=None):
         abort(404)
 
 
-@app_views.route(
-    '/places/<place_id>', methods=['DELETE'],
-    strict_slashes=False)
-def del_place(place_id=None):
-    """ delete place"""
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place(place_id=None):
+    """ delete a place object """
     if storage.get(Place, place_id):
         storage.delete(storage.get(Place, place_id))
         storage.save()
@@ -47,10 +42,9 @@ def del_place(place_id=None):
         abort(404)
 
 
-@app_views.route(
-    '/cities/<city_id>/places', methods=['POST'],
-    strict_slashes=False)
-def post_place(city_id=None):
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def creat_place(city_id=None):
     """ post place """
     city = storage.get(City, city_id)
     if city:
@@ -73,17 +67,17 @@ def post_place(city_id=None):
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def put_place(place_id=None):
+def update_place(place_id=None):
     """ put place """
     place = storage.get(Place, place_id)
     if place:
         updated = request.get_json()
         if not updated:
             abort(400, "Not a JSON")
-        for k, v in updated.items():
-            if k not in ['id', 'created_at', 'updated_at',
-                        'user_id', 'city_id']:
-                setattr(place, k, v)
+        for key, val in updated.items():
+            if key not in ['id', 'created_at', 'updated_at',
+                           'user_id', 'city_id']:
+                setattr(place, key, val)
         storage.save()
         return jsonify(place.to_dict()), 200
     abort(404)
