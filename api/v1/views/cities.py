be35c4382fcd541f78a39973cc4460e74e1e19af
Fares84 <fares_sassi@outlook.fr>
2021-05-10 12:04:25 +0000

    fixing file with correct id added

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""create a new view for City objects that handles all default RestFul API."""
+""" creates a new view fo City to handle all default Restful API actions """
 
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
@@ -10,7 +10,7 @@ from models.state import State
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-def ret_allcities(state_id):
+def states_cities(state_id):
     """retrieve all City object of a State"""
     state = storage.get('State', state_id)
     if state is None:
@@ -23,8 +23,8 @@ def ret_allcities(state_id):
 
 @app_views.route('/cities/<string:city_id>', methods=['GET'],
                  strict_slashes=False)
-def retr_city(city_id):
-    """retrieve information from a given city"""
+def allcities(city_id):
+    """ retrieve information from a given city """
     city = storage.get('City', city_id)
     if city is None:
         abort(404)
@@ -34,7 +34,7 @@ def retr_city(city_id):
 @app_views.route('/cities/<string:city_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_city(city_id):
-    """delete a City object"""
+    """ delete a City obj """
     city = storage.get('City', city_id)
     if city is None:
         abort(404)
@@ -45,7 +45,7 @@ def delete_city(city_id):
 
 @app_views.route('/states/<string:state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-def creat_obj(state_id):
+def create_obj(state_id):
     """create an object"""
     state = storage.get("State", state_id)
     if state is None:
@@ -64,14 +64,14 @@ def creat_obj(state_id):
 @app_views.route('/cities/<string:city_id>', methods=['PUT'],
                  strict_slashes=False)
 def new_obj(city_id):
-    """update an existing object"""
+    """ update an existing object """
     city = storage.get('City', city_id)
     if city is None:
         abort(404)
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for k, v in request.get_json().items():
-        if k not in ['id', 'created_at', 'updated_at']:
-            setattr(city, k, v)
+    for key, val in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(city, key, val)
     city.save()
     return jsonify(city.to_dict())
