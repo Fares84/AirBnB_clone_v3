b45aa91ca24b835507b077beca216f7ff3f92203
Fares84 <fares_sassi@outlook.fr>
2021-05-10 10:48:15 +0000

    added files city amenity user place

---
+++ api/v1/views/places.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" City objs that handles all default Restful API actions """
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from models import storage
+from models.place import Place
+from models.city import City
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
+def place_list(city_id):
+    """ retrieve all Place list object of a City """
+    city = storage.get('City', city_id)
+    if city is None:
+        abort(404)
+    places = []
+    for place in city.places:
+        places.append(place.to_dict())
+    return jsonify(places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_place(place_id):
+    """ retrieve information from a given place """
+    place = storage.get('Place', place_id)
+    if place is None:
+        abort(404)
+    return jsonify(place.to_dict())
+
+
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
+def delete_place(place_id):
+    """ delete a place objects """
+    place = storage.get('Place', place_id)
+    if place is None:
+        abort(404)
+    place.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def create_place(city_id):
+    """ create an object """
+    kwargs = request.get_json()
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if user_id not in kwargs:
+        return make_response(jsonify({'error': 'Missing user_id'}), 400)
+    user = storage.get("User", kwargs['user_id'])
+    if user is None:
+        abort(404)
+    if 'name' not in kwargs:
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    kwargs['city_id'] = city_id
+    place = Place(**kwargs)
+    place.save()
+    return make_response(jsonify(place.to_dict()), 201)
---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+""" creates a new view to handle all default Restful API actions """
